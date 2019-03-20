b89ff7d3d5c2c4fa1c7b0bc5ef6ce7b6c18c3dc1
onhyh <45840152+onhyh@users.noreply.github.com>
2019-03-20 17:06:47 +0900

    Add files via upload

--- assignment1/processData.m
+++ assignment1/processData.m
@@ -12,82 +12,24 @@ clc
 % command to load dataForTesting.mat
 load dataForTesting
 data = fixData(data);
-% gets each column
-c1 = data(:,1);
-c2 = data(:,2);
-c3 = data(:,3);
-c4 = data(:,4);
-c5 = data(:,5);
-c6 = data(:,6);
-c7 = data(:,7);
-c8 = data(:,8);
-c9 = data(:,9);
-c10 = data(:,10);
-c11 = data(:,11);
-c12 = data(:,12);
-c13 = data(:,13);
-c14 = data(:,14);
- 
 %creates figure
 f3 = figure(3);
 set(f3, 'Units', 'pixels', 'Position', [0 0 1000 1000]);
  
 %subplots
-subplot(3,5,1);
-scatter(c1,c14);
-title('Column 1');
- 
- 
-subplot(3,5,2);
-scatter(c2,c14);
-title('Column 2');
- 
-subplot(3,5,3);
-scatter(c3,c14);
-title('Column 3');
- 
-subplot(3,5,4);
-scatter(c4,c14);
-title('Column 4');
- 
-subplot(3,5,5);
-scatter(c5,c14);
-title('Column 5');
- 
-subplot(3,5,6);
-scatter(c6,c14,[],'r');
-title('Column 6');
- 
-subplot(3,5,7);
-scatter(c7,c14);
-title('Column 7');
- 
-subplot(3,5,8);
-scatter(c8,c14,[],'r');
-title('Column 8');
- 
-subplot(3,5,9);
-scatter(c9,c14);
-title('Column 9');
- 
-subplot(3,5,10);
-scatter(c10,c14);
-title('Column 10');
- 
-subplot(3,5,11);
-scatter(c11,c14);
-title('Column 11');
- 
-subplot(3,5,12);
-scatter(c12,c14);
-title('Column 12');
- 
-subplot(3,5,13);
-scatter(c13,c14,[],'r');
-title('Column 13');
- 
-sgtitle('Comparison of Each Col with Col 14');
+red_col = [6 8 13];
 
+for x = 1:1:13
+	subplot(3,5,x);
+	if ismember(x, red_col)
+    	scatter(data(:,x),data(:,14),[], 'r');
+	else
+    	scatter(data(:,x),data(:,14));
+	end
+	title("Column " + num2str(x));
+end
+ 
+sgtitle('Comparison of Each Col with Col 14')
 
 % We believe that columns 6, 8 and 13 have the most explanatory power for 
 % determining the final column (column 14) of data. We plotted 13 scatter
@@ -96,3 +38,4 @@ sgtitle('Comparison of Each Col with Col 14');
 % correlation with column 14 and column 13 a negative correlation. Other
 % columns do not seem to have a clear and consistent trend with column 14 
 % or has too many outliers. 
+
