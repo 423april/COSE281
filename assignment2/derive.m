3941d05c114114be7def8b43c58fdf6512e6d41a
onhyh <45840152+onhyh@users.noreply.github.com>
2019-04-09 17:07:53 +0900

    Update derive.m

--- assignment2/derive.m
+++ assignment2/derive.m
@@ -1,8 +1,10 @@
 % COSE281 ENGINEERING MATHEMATICS 2019 ASSIGNMENT 2 PROBLEM #1
 % STUDENT IDs: 2015320143, 2016320128, 2018320250
 % 
-% derive 
-%
+% derive Check and fix data input with some error handling.
+%   fixData(DATA) checks if the data input conforms to the expected
+%   dimensions of 206-by-14 and fixes the input such that all values in the
+%   data are positive or 0 by deleting rows with negative or NaN values.
 %   
 %   INPUT:
 %       function_handle: variable representing function
@@ -12,7 +14,10 @@
 %   OUTPUT:
 %       derivative: 
 %
-%   EXAMPLE: 
+%   EXAMPLE: Fix the data in variable DATA and save the output back into
+%            DATA.
+%
+%       DATA = fixData(DATA);
 
 function [derivative] = derive(function_handle, x_values, h)
 
@@ -21,7 +26,7 @@ if (nargin == 2)
     h = 1e - 5;
 end
 % this does the job also for arrays of inputs
-derivative = (f(x + h)-f(x))/h;
+derivative = (function_handle(x_values + h) - function_handle(x_values)) / h;
 
 
 
