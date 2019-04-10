2e92db2b50fd2ce5baff8d102cc8350de0879697
423april <423april@naver.com>
2019-04-10 12:08:52 +0900

    Update integrate.m

--- assignment2/integrate.m
+++ assignment2/integrate.m
@@ -1,61 +1,52 @@
 % COSE281 ENGINEERING MATHEMATICS 2019 ASSIGNMENT 2 PROBLEM #2
 % STUDENT IDs: 2015320143, 2016320128, 2018320250
 % 
-% integrate(function_handle, x_values, h, type) implements numerical
-% integration on a known function. The function evaluates the trapezoidal
-% or midpoint integral approximation for each pair of successive x-values.
-% It returns length(x_values) - 1 values. 
-% If h >= average_i (x_(i+1) - x_i), the function throws an error.
+% integrate
 %
 %   INPUT:
 %       function_handle: variable representing function
 %       x_values: array of x values
 %       h: optional, if not supplied should be set to h = 1e - 5
-%       type: optional, can be 'trapezoid' or 'midpoint' for respective
-%             integral approximatio rule, if not supplied set to 'trapezoid'
-%
+%       type:
 %   OUTPUT:
-%       integral: array of integrals for each pair of successive x-values, 
-%                 number of array elements is length(x_values) - 1
+%       integral: 
 %
-%   EXAMPLE: do a trapezoidal integral approximation for f = @(x) x^2, 
-%            x_values = [0.001 : 0.1 : 2]. Save the return value to
-%            variable integral
-%
-%       integral = integrate(f, x_values)
+%   EXAMPLE: 
 
 function [integral] = integrate(function_handle, x_values, h, type)
 
-% %% the guideline says to check val nargin but not sure how that'll work
-% for when h's missing but type's not
-
-% third argument h is optional. if not supplied, set to 1e-5
+% The third argument h should be optional
+% The fourth argument type should be optional. If not supplied, set to
+% 'trapezoid'.
 if ~exist('h', 'var')
     h = 1e-5;
 end
-
-% fourth argument type is optional. If not supplied, set to 'trapezoid'
 if ~exist('type', 'var')
     type = 'trapezoid';
 end
 
-% if type == 'trapezoid', do trapezoidal integral approximation
+
+% The fourth argument type is a string (or char array) with possible values ‘trapezoid’ and ’midpoint’
+% The function should evaluate the integral for each PAIR of SUCCESSIVE x-values 
+% and so will return length(x_values)-1 values
 if(strcmp(type, 'trapezoid') == 1)
-    for i = 1: length(x_values) - 1
+    area = zeros(1, length(x_values));
+    for x = 1: length(x_values) - 1
         % guarantee that h < average_i(x_i+1 - x_i) and throw an error if it doesn’t
-        assert(h < x_values(i+1) - x_values(i));  % not sure if this is right
+        dif = x_values(x+1) - x_values(x);
+        assert(h < dif);  % not sure if this is right
         
         % evaluate the integral for each PAIR of SUCCESSIVE x-values 
-        area(x) = (x_values(i+1)-x_vlaues(i))/2*(function_handle(x_values(i+1))+f(x_values(i))); 
+        area(x) = (x_values(x+1)-x_values(x))/2*(function_handle(x_values(x+1))+function_handle(x_values(x))); 
     end
     % return length(x_values)-1 values
     integral = area;
     
-% if type == 'midpoint', do midpoint integral approximation
- elseif(strcmp(type, 'midpoint') == 1)
-    for i = 1: length(x_values) - 1
-        assert(h < x_values(i+1) - x_values(i));
-        area(x) = area + ((x_values(i+1) - x_values(i)) * function_handle((x_values(i+1) + x_values(i))/2));
+elseif(strcmp(type, 'midpoint') == 1)
+    area = zeros(1, length(x_values));
+    for x = 1: length(x_values) - 1
+        assert(h < x_values(x+1) - x_values(x));
+        area(x) = area + ((x_values(x+1) - x_values(x)) * function_handle((x_values(x+1) + x_values(x))/2));
     end
     integral = area;
 end
