b9a7f13f5e495ec83e430ee8fd5a3da4f45e743b
423april <423april@naver.com>
2019-04-10 13:51:05 +0900

    Update testIntegrate.m

--- assignment2/testIntegrate.m
+++ assignment2/testIntegrate.m
@@ -1,9 +1,10 @@
-% COSE281 ENGINEERING MATHEMATICS 2019 ASSIGNMENT 2 PROBLEM #2
+% COSE281 ENGINEERING MATHEMATICS 2019 ASSIGNMENT 2 PROBLEM #1
 % STUDENT IDs: 2015320143, 2016320128, 2018320250
 % 
+% testIntegrate.m:
 % This script plots the SSE for all points summed up for h = 10^[-2 : -1 :
-% -7] and x_values = [0.001 : 0.1 : 2] for both trapezoidal and midpoint
-% integral approximations of the following function: f(x) = x^2 
+% -7] and x_values = [0.001 : 0.1 : 2] for the following function: 
+% f(x) = x^2 for both trapezoid and midpoint integration.
 
 % clear the workspace, close all figures and clear the output window 
 % of Matlab.
@@ -16,34 +17,28 @@ h = 10.^[-2 : -1 : -7];
 x_values = [0.001 : 0.1 : 2];
 
 % define symbol x and function f
-syms x 
+syms x
 f = @(x) x.^2;
 
-% definite integral of x^2 is x^3/3 
-% go through each element and calculate SSE of all points for trapezoidal
-% integral approximation
-for i = 1 : length(h)
-    trap_err(i) = sum((((x_values .^ 3) ./3) - integrate(f, x_values, h, 'trapezoid')).^2);
+for k = 1 : length(x_values) - 1
+    defint(k) = x_values(k + 1) .^3 .*(1/3) - x_values(k) .^3 .*(1/3);
 end
 
-% plot trap_err
-semilogy(trap_err) 
+% plot the trapezoid integration for  f(x) = x^2
+for i = 1 : length(h)
+    
+    err1(i) = sum((defint - integrate(f, x_values, h(i), 'trapezoid')).^2);
+end
+%err = sum((1/3) .* x_values .^3 - trapz(, f));
+semilogy(err1);
 
-% display grid lines, label x & y-axes, and retain plot in current axes
-grid on
-xlabel('h'); 
-ylabel('SSE [log scale]');
 hold on
 
-% go through each element and calculate SSE of all points for midpoint
-% integral approximation
+% plot the trapezoid integration for  f(x) = x^2
+
+
 for i = 1 : length(h)
-    mid_err(i) = sum((((x_values .^ 3) ./3) - integrate(f, x_values, h, 'midpoint')).^2);
+    err2(i) = sum((defint - integrate(f, x_values, h(i), 'midpoint')).^2);
 end
-
-% plot trap_err
-semilogy(mid_err)
-
-% Questions
-% 1) What happens to the error curves as h becomes smaller and why?
-% 2) Which integration method is better? 
+%err = sum((1/3) .* x_values .^3 - trapz(, f));
+%plot(h, err2);
