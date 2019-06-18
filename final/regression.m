c4ed35406e76dcca0e11f53879e00e4fcb8cd907
423april <423april@naver.com>
2019-06-18 22:25:46 +0900

    Update regression.m

--- final/regression.m
+++ final/regression.m
@@ -2,21 +2,39 @@ clear all
 close all
 clc
 
+% import data
 load abalone.mat
 data = table2array(abalone(:, 2:9));
-data = normalize(data);
+
+% shuffle data 
 shuffled = data;
-perm = randperm(size(data,1), round(size(data,1)*0.7));
-% for i = 1 : size(data,1)
-%     shuffled(perm(i), :)
-lm = fitlm(data(:,1:7), data(:, 8))
-coeff = table2array(lm.Coefficients(:, 1));
-pred = [ones(size(data, 1), 1) data(:, 1:7)] * coeff;
-sse = sum((data(:, 8) - pred) .^ 2) / 4177;
+perm = randperm(size(data,1));
+for i = 1 : size(data,1)
+    shuffled(perm(i), :) = data(i,:);
+end
 
-newdata = data(:, 2:8);
-lm = fitlm(newdata(:,1:6), newdata(:, 7))
+% normalize data
+ndata = normalize(shuffled);
 
-plot(lm)
+% devide normalized data set into training set and test set
+train = ndata(1 : round(size(ndata, 1) * 0.7), :);
+test = ndata(round(size(ndata, 1) * 0.7) + 1 : size(ndata,1), :);
 
+% use normalized data to calculate model
+ncoeff = [ones(size(train, 1), 1) train(:,1:7)]\train(:,8)
+
+pred = [ones(size(test,1), 1) test(:, 1:7)] * ncoeff;
+mse = sum((test(:,8) - pred) .^ 2) / size(test,1);
+
+% devide data set into training set and test set
+train = shuffled(1 : round(size(shuffled, 1) * 0.7), :);
+test = shuffled(round(size(shuffled, 1) * 0.7) + 1 : size(shuffled,1), :);
+
+% calculate model
+coeff = [ones(size(train, 1), 1) train(:,1:7)]\train(:,8);
+
+% calculate mse and sse on test set
+pred = [ones(size(test,1), 1) test(:, 1:7)] * coeff;
+mse = sum((test(:,8) - pred) .^ 2) / size(test,1)
+sse = sum((test(:,8) - pred) .^ 2)
 
